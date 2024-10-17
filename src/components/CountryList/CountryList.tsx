cb0bd6186690043cc3899e4a879a2004c4c7d85f
aibeknet <aaskarov0690.kg@gmail.com>
2024-10-17 13:56:06 +0600

    The types.ts file was created. I wrote interface Country for my data. I also added the Countrylist.tsx component.This component will display a list of countries.

---
+++ src/components/CountryList/CountryList.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Country } from '../../types';
+
+interface CountryListProps {
+  countries: Country[];
+  onCountrySelect: (alpha3Code: string) => void;
+}
+
+const CountryList: React.FC<CountryListProps> = ({ countries, onCountrySelect }) => {
+  return (
+    <div className="overflow-auto" style={{ maxHeight: '400px' }}>
+      <ul className="list-group">
+        {countries.map((country) => (
+          <li
+            key={country.alpha3Code}
+            className="#"
+            onClick={() => onCountrySelect(country.alpha3Code)}
+            style={{ cursor: 'pointer' }}
+          >
+            <img
+              src={country.flag}
+              alt={`${country.name} flag`}
+              style={{ width: '30px', height: '20px', marginRight: '10px' }}
+            />
+            {country.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CountryList;
+
+//#list-group-item d-flex justify-content-between align-items-center
